f9d265d15762afc34d6565a1b7b8d5bd9c739b01
gianhunold <hunoldgian@outlook.de>
2022-09-03 10:42:17 +0200

    Update 0.08 Added Basic Buy/Sell

--- js/marketupdate.js
+++ js/marketupdate.js
@@ -1,3 +1,5 @@
+const anteile = [0,0,0]
+
 function marketupdate() {
     setInterval(priceUpdate, 1000);
 }
@@ -39,10 +41,13 @@ function priceUpdate() {
     }
 }
 
-function changeView_Aktie(aktienname, preis) {
+function changeView_Aktie(listid) {
     document.getElementById("market").style.display = "none";
     document.getElementById("news").style.display = "none";
     document.getElementById("aktienansicht").style.display = "block";
+
+    var aktienname = document.getElementById("marketview").rows[listid].cells[0].innerHTML;
+    var preis = Number(document.getElementById("marketview").rows[listid].cells[1].innerHTML);
 
     document.getElementById("aktienname").innerHTML = aktienname;
     document.getElementById("aktienpreis").innerHTML = preis;
@@ -60,10 +65,31 @@ function aktiekaufen() {
     if (kontostand > aktienpreis) {
         kontostand = kontostand - aktienpreis;
         document.getElementById("account").innerHTML = kontostand;
+        anteile[0] = anteile[0] + 1;
     }
 
     if (kontostand < aktienpreis) {
         insuficentfunds();
     }
 
+}
+
+function aktieverkaufen() {
+    var aktienpreis = Number(document.getElementById("aktienpreis").innerHTML);
+    var kontostand = Number(document.getElementById("account").innerHTML);
+
+    if (anteile[0] >= 1) 
+    {
+        kontostand = kontostand + aktienpreis;
+        document.getElementById("account").innerHTML = kontostand;
+        anteile[0] = anteile[0] - 1;
+    }
+}
+
+function exitaktie() {
+    document.getElementById("market").style.display = "block";
+    document.getElementById("news").style.display = "block";
+    document.getElementById("aktienansicht").style.display = "none";
+
+    document.getElementById("marketview").rows[1].cells[2].innerHTML = anteile[0];
 }
