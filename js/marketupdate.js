ea9b395053cc27e8584a6fe2b98eb827b8d06c5f
gianhunold <hunoldgian@outlook.de>
2022-09-04 09:51:12 +0200

    Update 0.10 Added a price graph, but only for the first

--- js/marketupdate.js
+++ js/marketupdate.js
@@ -1,5 +1,7 @@
 const anteile = [0,0,0,0,0,0] /*First Place in Array not used*/
 
+const aktie1prices = [0,0,0,0,0,0,0,0,0,0]
+
 function marketupdate() {
     setInterval(priceUpdate, 1000, 1);
     setInterval(priceUpdate, 1000, 2);
@@ -14,7 +16,7 @@ function priceUpdate(listid) {
     var currentprice = Number(priceTable.rows[listid].cells[1].innerHTML);
     var newprice = 0;
 
-    var change = Math.floor(Math.random() * 10);
+    var change = Math.round(Math.random() * 10);
     var changedirection = Math.floor(Math.random() * 2);
     if (changedirection == 0)
     {
@@ -43,6 +45,20 @@ function priceUpdate(listid) {
         priceTable.rows[listid].cells[1].innerHTML = newprice;
         priceTable.rows[listid].cells[1].classList.add("pricedec");
     }
+
+    if (listid == 1)
+    {
+        aktie1prices[0] = aktie1prices[1];
+        aktie1prices[1] = aktie1prices[2];
+        aktie1prices[2] = aktie1prices[3];
+        aktie1prices[3] = aktie1prices[4];
+        aktie1prices[4] = aktie1prices[5];
+        aktie1prices[5] = aktie1prices[6];
+        aktie1prices[6] = aktie1prices[7];
+        aktie1prices[7] = aktie1prices[8];
+        aktie1prices[8] = aktie1prices[9]; 
+        aktie1prices[9] = newprice;
+    }
 }
 
 function changeView_Aktie(listid) {
@@ -52,6 +68,7 @@ function changeView_Aktie(listid) {
 
     /*Add functional Elements*/
     document.getElementById("aktienansicht").innerHTML += "<h2 id='aktienname'></h2>";
+    document.getElementById("aktienansicht").innerHTML += "<canvas id='aktiengraph'></canvas>"
     document.getElementById("aktienansicht").innerHTML += "<button id='back' onclick='exitaktie(" + listid + ")'>Zurück</button>";
     document.getElementById("aktienansicht").innerHTML += "<button id='aktieverkaufen' onclick='aktieverkaufen(" + listid +")'>Verkaufen</button>";
     document.getElementById("aktienansicht").innerHTML += "<button id='aktiekaufen' onclick='aktiekaufen(" + listid + ")'>Kaufen</button>";
@@ -62,6 +79,8 @@ function changeView_Aktie(listid) {
 
     document.getElementById("aktienname").innerHTML = aktienname;
     document.getElementById("aktienpreis").innerHTML = preis;
+
+    setInterval(updateaktienview, 1000, listid);
 }
 
 function insuficentfunds() {
@@ -73,7 +92,7 @@ function aktiekaufen(listid) {
     var aktienpreis = Number(document.getElementById("aktienpreis").innerHTML);
     var kontostand = Number(document.getElementById("account").innerHTML);
 
-    if (kontostand > aktienpreis) {
+    if (kontostand >= aktienpreis) {
         kontostand = kontostand - aktienpreis;
         document.getElementById("account").innerHTML = kontostand;
         anteile[listid] = anteile[listid] + 1;
@@ -105,4 +124,39 @@ function exitaktie(listid) {
     document.getElementById("aktienansicht").innerHTML = "";
 
     document.getElementById("marketview").rows[listid].cells[2].innerHTML = anteile[listid];
+}
+
+function drawgraph() {
+    var canvas = document.getElementById("aktiengraph");
+
+    var ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, 250, 150);
+    var i = 0;
+    while (i < 10)
+    {
+        ctx.beginPath();
+        ctx.moveTo(i * 25, 150 - aktie1prices[i]);
+        ctx.lineTo((i+1) * 25, 150 - aktie1prices[i+1]);
+        if (aktie1prices[i+1] > aktie1prices[i])
+        {
+            ctx.strokeStyle = '#49FF00';
+        }
+        else if (aktie1prices[i+1] < aktie1prices[i])
+        {
+            ctx.strokeStyle = '#FF0000';
+        }
+        else if (aktie1prices[i+1] == aktie1prices[i])
+        {
+            ctx.strokeStyle = '#000000';
+        }
+        ctx.stroke();
+        i++;
+    }
+}
+
+function updateaktienview(listid) {
+    drawgraph();
+    var preis = Number(document.getElementById("marketview").rows[listid].cells[1].innerHTML);
+    document.getElementById("aktienpreis").innerHTML = preis;
 }
